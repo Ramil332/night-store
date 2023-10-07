c0dcd7dcd771c3d0a09a66dbbed0d5de0f9c4c30
croissantbro <valeraplay04@gmail.com>
2023-10-08 01:12:26 +0300

    Перемещение, вращение камеры, прицел(пока временный)

--- Assets/croissantbro/Scripts/PlayerMovement.cs
+++ Assets/croissantbro/Scripts/PlayerMovement.cs
@@ -1,38 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System;
+
 
     public class PlayerMovement : MonoBehaviour
     {
 
-    [SerializeField] private float _moveSpeed;
-    private Controls _controls;
+        public CharacterController controller;
+        public float speed = 12f;
+        public float gravity = -9.81f;
+        
+       
+        Vector3 velocity;
+      
+        public bool PlayerMove = true;
+      
 
-    private void Awake()
-    {
-        _controls = new Controls();
-    }
-    private void OnEnable()
-    {
-        _controls.Enable();
-    }
-    private void OnDisable()
-    {
-        _controls.Disable();
-    }
+        void Start()
+        {
+            
+        }
 
-    private void Update()
-    {
-        Vector2 moveDirection = _controls.Main.Move.ReadValue<Vector2>();
-        Move(moveDirection);
-    }
+        
+        
+        void Update()
+        {
+            
+            if (PlayerMove)
+            {
+
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
+
+                Vector3 move = transform.right * x + transform.forward * z;
+
+                controller.Move(move * speed * Time.deltaTime);
+
+                velocity.y += gravity * Time.deltaTime;
+
+                controller.Move(velocity * Time.deltaTime);
+
+        }
     
-    private void Move(Vector2 directionMove) 
-    {
-        Vector3 direction = new Vector3(directionMove.x, 0, directionMove.y);
-        transform.position += direction * _moveSpeed * Time.deltaTime;
     }
-
 }
