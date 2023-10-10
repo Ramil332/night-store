dafb56b15099f1489b285b36f5da81e8036212a8
ShroomikOrigin <udjolen@gmail.com>
2023-10-11 01:28:51 +0300

    Свет включается после ремонта генератора

--- Assets/Shroomik/Scripts/GlobalEvents.cs
+++ Assets/Shroomik/Scripts/GlobalEvents.cs
@@ -8,12 +8,14 @@ public class GlobalEvents : MonoBehaviour
     [Header("ƒлительность уровн€ в сек")]
     [SerializeField] [Range(0, 900)] private float _nightDuration;
 
-    [Header("ћин врем€ до след событи€")]
+    [Header("ћин врем€ до след событи€ в сек")]
     [SerializeField] [Range(0, 300)] private float _minNextEventTime;
-    [Header("ћакс врем€ до след событи€")]
+    [Header("ћакс врем€ до след событи€ в сек")]
     [SerializeField] [Range(0, 300)] private float _maxNextEventTime;
 
     public static Action OnCustomerSpawn;
+    public static Action OnGeneratorBroke;
+
 
     private int _nightTick;
     private int _nextEventTick;
@@ -53,11 +55,15 @@ public class GlobalEvents : MonoBehaviour
 
     private void CreateEvent()
     {
-        _eventNum = UnityEngine.Random.Range(0, 1); // здесь максимум это число общих событий
+        _eventNum = UnityEngine.Random.Range(0, 2); // здесь максимум это число общих событий
         switch (_eventNum)
         {
             case 0:
                 OnCustomerSpawn?.Invoke();
+                break;
+
+            case 1:
+                OnGeneratorBroke?.Invoke();
                 break;
 
             default:
