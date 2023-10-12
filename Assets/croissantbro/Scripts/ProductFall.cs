ebafa9a88795c79d96cd52c2a94d4f0bb5d04d86
croissantbro <valeraplay04@gmail.com>
2023-10-13 01:43:54 +0300

    фикс

--- Assets/croissantbro/Scripts/ProductFall.cs
+++ Assets/croissantbro/Scripts/ProductFall.cs
@@ -7,15 +7,14 @@ public class ProductFall : MonoBehaviour
     public List<GameObject> Products;
     private int _num = 0;
 
-    private void Update()
+    public void FallProducts()
     {
-        if (Input.GetKeyDown(KeyCode.H))
-        {
+        
 
             Vector3 currentRotation = Products[_num].transform.eulerAngles;
             Products[_num].transform.eulerAngles = new Vector3(-30, currentRotation.y, currentRotation.z);
             _num++;
-        }
+        
                 
         
     }
